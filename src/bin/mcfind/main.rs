1d144f9f26b076e161d6d253623a4a58ad37c823
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2020-08-04 19:31:21 +0200

    Nicer error chaining

--- src/bin/mcfind/main.rs
+++ src/bin/mcfind/main.rs
@@ -1,34 +1,42 @@
 mod cli;
 
-use cli::Error;
-use std::error::Error as StdError;
+use cli::Error as CliError;
+use std::error::Error;
 
 fn main() {
     if let Err(e) = cli::run() {
         eprintln!("Error: {}", e);
 
-        chain(&e)
+        e.chain()
             .skip(1)
             .take(1)
             .for_each(|c| eprintln!("\nCaused by:\n    {}", c));
 
         match e {
-            Error::NoCommand | Error::MissingArgumentError(_) => println!("\n{}", cli::usage()),
+            CliError::NoCommand | CliError::MissingArgumentError(_) => {
+                println!("\n{}", cli::usage())
+            }
             _ => (),
         }
     }
 }
 
-fn chain(error: &dyn StdError) -> Chain {
-    Chain { next: Some(error) }
+trait ErrorChain {
+    fn chain(&self) -> Chain;
+}
+
+impl<T: Error> ErrorChain for T {
+    fn chain(&self) -> Chain {
+        Chain { next: Some(self) }
+    }
 }
 
 struct Chain<'a> {
-    next: Option<&'a dyn StdError>,
+    next: Option<&'a dyn Error>,
 }
 
 impl<'a> Iterator for Chain<'a> {
-    type Item = &'a dyn StdError;
+    type Item = &'a dyn Error;
 
     fn next(&mut self) -> Option<Self::Item> {
         let error = self.next?;
